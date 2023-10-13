37b06de52cba64efd0799c44d8904e23bf598adb
Maisie Ng <48564899+NG-MEIXI-MAISIE@users.noreply.github.com>
2023-10-13 12:17:22 +0800

    Upload the updated file

--- examples/pytorch/question-answering/hello_world.py
+++ examples/pytorch/question-answering/hello_world.py
@@ -1,34 +1,71 @@
-import logging
-import math
+
+#!/usr/bin/env python
+
+# hello.py
+
+# Environment Variables
+# https://github.com/aws/sagemaker-training-toolkit/blob/master/ENVIRONMENT_VARIABLES.md
+# SM_MODEL_DIR = '/opt/ml/model'
+# SM_CHANNELS = '["testing","training"]'
+# SM_CHANNEL_TRAINING='/opt/ml/input/data/training'
+# SM_CHANNEL_TESTING='/opt/ml/input/data/testing'
+
+import argparse
+import json
 import os
-import sys
-import warnings
-from dataclasses import dataclass, field
-from itertools import chain
-from typing import Optional
-
-import datasets
-import evaluate
-import torch
-from datasets import load_dataset
-
+import sacremoses
 import transformers
-from transformers import (
-    CONFIG_MAPPING,
-    MODEL_FOR_CAUSAL_LM_MAPPING,
-    AutoConfig,
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    HfArgumentParser,
-    Trainer,
-    TrainingArguments,
-    default_data_collator,
-    is_torch_tpu_available,
-    set_seed,
-)
-from transformers.testing_utils import CaptureLogger
-from transformers.trainer_utils import get_last_checkpoint
-from transformers.utils import check_min_version, send_example_telemetry
-from transformers.utils.versions import require_version
 
-print(pub2.json)
+print(transformers.__version__)
+print(sacremoses.__version__)
+
+
+def main(args):
+    print('===== H E L L O   W O R L D =====')
+    print()
+    print(f'channel_names = {args.channel_names}')
+    print(f'num_cpus = {args.num_cpus}')
+    print(f'num_gpus = {args.num_gpus}')
+    print(f'output_data_dir = {args.output_data_dir}')
+    print(f'model_dir = {args.model_dir}')
+    print(f'train_file = {args.train_file}')
+    print()
+
+    print('----- Train File -----')
+    train_file = args.train_file
+    if train_file.endswith('.json'):
+        with open(train_file, 'r') as f:
+            data = json.load(f)
+            
+            print(json.dumps(data, indent=2))
+
+    print('----- End of Train File -----')
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # https://sagemaker.readthedocs.io/en/stable/frameworks/pytorch/using_pytorch.html#train-a-model-with-pytorch
+    # parser.add_argument('--epochs', type=int, default=50)
+    # parser.add_argument('--batch-size', type=int, default=64)
+    # parser.add_argument('--learning-rate', type=float, default=0.05)
+    # parser.add_argument('--use-cuda', type=bool, default=False)
+
+    # SageMaker inputs
+    parser.add_argument('--channel_names', default=json.loads(os.environ['SM_CHANNELS']))
+    parser.add_argument('--num_cpus', type=int, default=os.environ['SM_NUM_CPUS'])
+    parser.add_argument('--num_gpus', type=int, default=os.environ['SM_NUM_GPUS'])
+    parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
+    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
+
+    # Example Hugging Face container command
+    # /opt/conda/bin/python3.9 hello.py --model_name_or_path microsoft/biogpt --output_dir /opt/ml/model --train_file /opt/ml/input/data/train/pub2.json
+    parser.add_argument('--model_name_or_path', type=str, default=os.environ['SM_HP_MODEL_NAME_OR_PATH'])
+    parser.add_argument('--output_dir', type=str, default=os.environ['SM_HP_OUTPUT_DIR'])
+    parser.add_argument('--train_file', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
+    # parser.add_argument('--test_file', type=str, default=os.environ['SM_CHANNEL_TEST'])
+    args, _ = parser.parse_known_args()
+
+    main(args)
+
