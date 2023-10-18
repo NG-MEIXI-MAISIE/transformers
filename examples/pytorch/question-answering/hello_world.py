09fe25518b0d86b0db75fc949d4968908a19bdd2
Maisie Ng <48564899+NG-MEIXI-MAISIE@users.noreply.github.com>
2023-10-18 14:22:13 +0800

    Update hello_world.py

--- examples/pytorch/question-answering/hello_world.py
+++ examples/pytorch/question-answering/hello_world.py
@@ -118,7 +118,7 @@ def main(args):
         output_dir="BioGPT-finetuned-GUSTO",
         evaluation_strategy = "epoch",
         learning_rate=2e-5,
-        weight_decay=0.01,)
+        weight_decay=0.01,
         push_to_hub=True)
 
 
