255af7a8f0e63589fc1bba43f186fc366df43d90
Maisie Ng <48564899+NG-MEIXI-MAISIE@users.noreply.github.com>
2023-11-08 11:52:44 +0800

    Update hello_world.py

--- examples/pytorch/question-answering/hello_world.py
+++ examples/pytorch/question-answering/hello_world.py
@@ -102,8 +102,8 @@ def main(args):
         total_length = len(concatenated_examples[list(examples.keys())[0]])
         # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
     # customize this part to your needs.
-        #if total_length >= block_size:
-            #total_length = (total_length // block_size) * block_size
+        if total_length >= block_size:
+            total_length = (total_length // block_size) * block_size
     # Split by chunks of block_size.
         result = {
             k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
